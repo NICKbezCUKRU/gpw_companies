1b5970e714e8b38f5983f8797919d203b8428520
Marcin Domański <71190674+NICKbezCUKRU@users.noreply.github.com>
2025-03-16 17:05:03 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import smtplib
 import pandas as pd
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
